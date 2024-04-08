8bb6eaf11ca50537524aa3f11bd9a176657c6e7a
DeadIzI <quentin.velard1@gmail.com>
2024-04-08 13:31:34 +1000

    bug extended_VQC

--- utility/extended_VQC.py
+++ utility/extended_VQC.py
@@ -4,59 +4,50 @@ from qiskit_machine_learning.utils.loss_functions import Loss, CrossEntropyLoss
 
 from qiskit_machine_learning.neural_networks import SamplerQNN
 
-    
-class CrossDistilledLoss(Loss):
-    def __init__(self, temperature=1.0, alpha=0.5):
-        super().__init__()
-        self.temperature = temperature
-        self.alpha = alpha
-        self.ce_loss = CrossEntropyLoss()
+from qiskit import QuantumCircuit
+from qiskit.primitives import BaseSampler
+from qiskit_algorithms.optimizers import Optimizer, OptimizerResult, Minimizer
 
-    def evaluate(self, student_output: np.ndarray, teacher_output: np.ndarray, target: np.ndarray, is_new_data: np.ndarray) -> np.ndarray:
-        self._validate_shapes(student_output, target)
-        self._validate_shapes(teacher_output, target)
-        self._validate_shapes(is_new_data, target)
-    
-
-        loss = np.zeros_like(target)
-
-        # Classification loss for new data
-        ce_loss = self.ce_loss.evaluate(student_output, target)
-        loss += is_new_data * ce_loss
-
-        # Distillation loss for old data
-        teacher_output_soft = np.exp(teacher_output / self.temperature) / np.sum(np.exp(teacher_output / self.temperature), axis=-1, keepdims=True)
-        student_output_soft = np.exp(student_output / self.temperature) / np.sum(np.exp(student_output / self.temperature), axis=-1, keepdims=True)
-
-        kl_loss = np.sum(student_output_soft * (np.log(student_output_soft) - np.log(teacher_output_soft)), axis=-1, keepdims=True) * (self.temperature ** 2)
-        loss += (1 - is_new_data) * kl_loss
-
-        # Combine losses
-        loss = self.alpha * ce_loss + (1 - self.alpha) * kl_loss
-        return loss
-
-    def gradient(self, student_output: np.ndarray, teacher_output: np.ndarray, target: np.ndarray, is_new_data: np.ndarray) -> np.ndarray:
-        self._validate_shapes(student_output, target)
-        self._validate_shapes(teacher_output, target)
-        self._validate_shapes(is_new_data, target)
-
-        gradient = np.zeros_like(student_output)
-
-        # Classification gradient for new data
-        ce_gradient = self.ce_loss.gradient(student_output, target)
-        gradient += is_new_data[:, np.newaxis] * ce_gradient
-
-        # Distillation gradient for old data
-        teacher_output_soft = np.exp(teacher_output / self.temperature) / np.sum(np.exp(teacher_output / self.temperature), axis=-1, keepdims=True)
-        student_output_soft = np.exp(student_output / self.temperature) / np.sum(np.exp(student_output / self.temperature), axis=-1, keepdims=True)
-
-        kl_gradient = (student_output_soft - teacher_output_soft) / self.temperature
-        gradient += (1 - is_new_data)[:, np.newaxis] * kl_gradient
-
-        # Combine gradients
-        gradient = self.alpha * ce_gradient + (1 - self.alpha) * kl_gradient
-          
-        return gradient
+from qiskit_machine_learning.utils import  derive_num_qubits_feature_map_ansatz
+from typing import Callable
+from qiskit_machine_learning.algorithms.classifiers import VQC
+from utility.froze import FrozenQNN   
 
 
+class ExtendedVQC(VQC):
+    def __init__(
+        self,
+        num_qubits: int | None = None,
+        feature_map: QuantumCircuit | None = None,
+        ansatz: QuantumCircuit | None = None,
+        loss: str | Loss = "cross_entropy",
+        optimizer: Optimizer | Minimizer | None = None,
+        warm_start: bool = False,
+        initial_point: np.ndarray | None = None,
+        callback: Callable[[np.ndarray, float], None] | None = None,
+        *,
+        sampler: BaseSampler | None = None,
+    ) -> None:
+
+        num_qubits, feature_map, ansatz = derive_num_qubits_feature_map_ansatz(
+            num_qubits, feature_map, ansatz
+        )
+
+        # construct circuit
+        self._feature_map = feature_map
+        self._ansatz = ansatz
+        self._num_qubits = num_qubits
+        self._circuit = QuantumCircuit(self._num_qubits)
+        self._circuit.compose(self.feature_map, inplace=True)
+        self._circuit.compose(self.ansatz, inplace=True)
+
+        neural_network = FrozenQNN(
+            sampler=sampler,
+            circuit=self._circuit,
+            input_params=self.feature_map.parameters,
+            weight_params=self.ansatz.parameters,
+            interpret=self._get_interpret(2),
+            output_shape=2,
+            input_gradients=False,
+        )
 
--- utility/froze.py
+++ utility/froze.py
@@ -37,93 +37,8 @@ else:
 
 logger = logging.getLogger(__name__)
 
-class Frozen(SamplerQNN):
-    def __init__(
-        self,
-        *,
-        circuit: QuantumCircuit,
-        sampler: BaseSampler | None = None,
-        input_params: Sequence[Parameter] | None = None,
-        weight_params: Sequence[Parameter] | None = None,
-        sparse: bool = False,
-        interpret: Callable[[int], int | tuple[int, ...]] | None = None,
-        output_shape: int | tuple[int, ...] | None = None,
-        gradient: BaseSamplerGradient | None = None,
-        input_gradients: bool = False,
-    ):
-        """
-        Args:
-            sampler: The sampler primitive used to compute the neural network's results.
-                If ``None`` is given, a default instance of the reference sampler defined
-                by :class:`~qiskit.primitives.Sampler` will be used.
-            circuit: The parametrized quantum circuit that generates the samples of this network.
-                If a :class:`~qiskit_machine_learning.circuit.library.QNNCircuit` is passed, the
-                `input_params` and `weight_params` do not have to be provided, because these two
-                properties are taken from the
-                :class:`~qiskit_machine_learning.circuit.library.QNNCircuit`.
-            input_params: The parameters of the circuit corresponding to the input. If a
-                :class:`~qiskit_machine_learning.circuit.library.QNNCircuit` is provided the
-                `input_params` value here is ignored. Instead the value is taken from the
-                :class:`~qiskit_machine_learning.circuit.library.QNNCircuit` input_parameters.
-            weight_params: The parameters of the circuit corresponding to the trainable weights. If
-                a :class:`~qiskit_machine_learning.circuit.library.QNNCircuit` is provided the
-                `weight_params` value here is ignored. Instead the value is taken from the
-                :class:`~qiskit_machine_learning.circuit.library.QNNCircuit` weight_parameters.
-            sparse: Returns whether the output is sparse or not.
-            interpret: A callable that maps the measured integer to another unsigned integer or
-                tuple of unsigned integers. These are used as new indices for the (potentially
-                sparse) output array. If no interpret function is
-                passed, then an identity function will be used by this neural network.
-            output_shape: The output shape of the custom interpretation. It is ignored if no custom
-                interpret method is provided where the shape is taken to be
-                ``2^circuit.num_qubits``.
-            gradient: An optional sampler gradient to be used for the backward pass.
-                If ``None`` is given, a default instance of
-                :class:`~qiskit_algorithms.gradients.ParamShiftSamplerGradient` will be used.
-            input_gradients: Determines whether to compute gradients with respect to input data.
-                 Note that this parameter is ``False`` by default, and must be explicitly set to
-                 ``True`` for a proper gradient computation when using
-                 :class:`~qiskit_machine_learning.connectors.TorchConnector`.
-        Raises:
-            QiskitMachineLearningError: Invalid parameter values.
-        """
-        # set primitive, provide default
-        if sampler is None:
-            sampler = Sampler()
-        self.sampler = sampler
-
-        # set gradient
-        if gradient is None:
-            gradient = ParamShiftSamplerGradient(self.sampler)
-        self.gradient = gradient
-
-        self._org_circuit = circuit
-
-        if isinstance(circuit, QNNCircuit):
-            self._input_params = list(circuit.input_parameters)
-            self._weight_params = list(circuit.weight_parameters)
-        else:
-            self._input_params = list(input_params) if input_params is not None else []
-            self._weight_params = list(weight_params) if weight_params is not None else []
-
-        if sparse:
-            _optionals.HAS_SPARSE.require_now("DOK")
-
-        self.set_interpret(interpret, output_shape)
-        self._input_gradients = input_gradients
-
-        super().__init__(
-            num_inputs=len(self._input_params),
-            num_weights=len(self._weight_params),
-            sparse=sparse,
-            output_shape=self._output_shape,
-            input_gradients=self._input_gradients,
-        )
-
-        if len(circuit.clbits) == 0:
-            circuit = circuit.copy()
-            circuit.measure_all()
-        self._circuit = self._reparameterize_circuit(circuit, input_params, weight_params)
+class FrozenQNN(SamplerQNN):
+    
 
     def _backward(
         self,
@@ -139,7 +54,7 @@ class Frozen(SamplerQNN):
         frozen_params = parameter_values[:, :n]
         trainable_params = parameter_values[:, n:]
         input_grad, weights_grad = None, None
-
+        print("hrllo")
         if np.prod(trainable_params.shape) > 0:
             circuits = [self._circuit] * num_samples
 
